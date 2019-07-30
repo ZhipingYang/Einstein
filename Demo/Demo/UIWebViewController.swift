02271dd7875197e825a8183d6183ae57d0da584d
XcodeYang <xcodeyang@gmail.com>
2019-07-31 01:15:25 +0800

    update story board

---
+++ Demo/Demo/UIWebViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UIWebViewController.swift
+//  Demo
+//
+//  Created by Daniel Yang on 2019/7/31.
+//  Copyright © 2019 Daniel Yang. All rights reserved.
+//
+
+import UIKit
+
+class UIWebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
